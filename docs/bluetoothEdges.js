811a7a77e0ecd228e21168efd6b0f6d4884449c0
Marco Mora-Mendoza <21184630+Marco-MM@users.noreply.github.com>
2019-08-19 13:32:29 -0700

    Update bluetoothEdges.js

--- docs/bluetoothEdges.js
+++ docs/bluetoothEdges.js
@@ -106,7 +106,7 @@ class HeartRateMonitor {
 }
 function handleCharacteristicValueChanged(event) {
   var value = event.target.value;
-  var buffer = new ArrayBuffer(22)
+  var buffer = new ArrayBuffer(22);
   console.log('Received ' + value(buffer, 0, 22));
   // TODO: Parse Heart Rate Measurement value.
   // See https://github.com/WebBluetoothCG/demos/blob/gh-pages/heart-rate-sensor/heartRateSensor.js
